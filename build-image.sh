6feb2a6f7f63a2bf9b0cb7711cf3ec0da0f2cb76
hbaocr <duonghuynhbaocr@gmail.com>
2022-12-22 22:56:48 +0700

    RMIT local network and Teacher PC

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,2 @@
-docker tag expedition-boot_node duonghuynhbaocr/expedition-local-nginx:0.0.1
-docker push duonghuynhbaocr/expedition-local-nginx:0.0.1
+docker tag expedition-boot_node duonghuynhbaocr/expedition-local-nginx:0.0.4
+docker push duonghuynhbaocr/expedition-local-nginx:0.0.4
--- set_default_chain.sh
+++ set_default_chain.sh
@@ -1,24 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
 #mv /blockexplorer/expedition/src/hooks/useChainList.ts ./useChainList.ts.origin
-CHAIN_NAME=${CHAIN_NAME:-"Ethereum Classic"}
-CHAIN_TYPE=${CHAIN_TYPE:-"mainnet"}
-NODE_RPC_URL=${NODE_RPC_URL:-"https://www.ethercluster.com/etc"}
+# CHAIN_NAME=${CHAIN_NAME:-"Ethereum Classic"}
+# CHAIN_TYPE=${CHAIN_TYPE:-"mainnet"}
+# NODE_RPC_URL=${NODE_RPC_URL:-"https://www.ethercluster.com/etc"}
 #/blockexplorer/expedition/src/hooks/useChainList.ts
 cat << EOF > src/hooks/useChainList.ts
 
 import { IChain as Chain } from "../models/chain";
 import React from "react";
 
-const chain_name=$CHAIN_NAME
-const chain_type=$CHAIN_TYPE
-const chain_rpc=$NODE_RPC_URL
-
 export default function() {
+  let rpc_url="";
+   if (typeof window !== 'undefined') {
+    rpc_url= "http://"+window.location.hostname+":9545";
+  }else{
+    rpc_url= "http://127.0.0.1:9545";
+  }
+
   const [chains, setChains] = React.useState<Chain[]>([
     {
       name: "RMIT Lab Coin",
-      network: "local",
+      network: "Lecturer's Node",
+      rpc: [rpc_url],
+    },
+     {
+      name: "RMIT Lab Coin(Local)",
+      network: "Local Node",
       rpc: ["http://127.0.0.1:9545"],
     }
   ]);
